00b648f863d065881bf0960418996e1cea0d3189
Kevin Fatkin <kfatkin@gmail.com>
2021-11-18 22:52:40 -0500

    Create image module and add influxdb

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
-resource "null_resource" "dockervol" {
-  provisioner "local-exec" {
-    command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
+locals {
+  deployment = {
+    nodered = {
+      image = var.image["nodered"][terraform.workspace]
+    }
+    influxdb = {
+      image = var.image["influxdb"][terraform.workspace]
+    }
   }
 }
 
 module "image" {
   source   = "./image"
-  image_in = var.image[terraform.workspace]
+  for_each = local.deployment
+  image_in = each.value.image
 }
+
 resource "random_string" "random" {
   count   = local.container_count
   length  = 4
@@ -16,19 +23,16 @@ resource "random_string" "random" {
 }
 
 
-resource "docker_container" "nodered_container" {
-  count = local.container_count
-  name  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image = module.image.image_out
-  ports {
-    internal = var.int_port
-    external = var.ext_port[terraform.workspace][count.index]
-  }
-  volumes {
-    container_path = "/data"
-    host_path      = "${path.cwd}/noderedvol"
-  }
+module "container" {
+  source            = "./container"
+  count             = local.container_count
+  name_in           = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
+  image_in          = module.image["nodered"].image_out
+  int_port_in       = var.int_port
+  ext_port_in       = var.ext_port[terraform.workspace][count.index]
+  container_path_in = "/data"
 }
+
 
 
 
