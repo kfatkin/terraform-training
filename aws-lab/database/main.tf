001ee34b6e3d286460eff65ed26fabf382f31ff4
Kevin Fatkin <kfatkin@gmail.com>
2021-12-09 20:19:21 -0500

    Add base database module

---
+++ aws-lab/database/main.tf
@@ -0,0 +1,20 @@
+# --- database/main.tf
+
+resource "aws_db_instance" "mtc_db" {
+  provider               = aws.target
+  engine                 = "mysql"
+  allocated_storage      = var.db_storage
+  engine_version         = var.db_engine_version
+  instance_class         = var.db_instance_class
+  name                   = var.dbname
+  username               = var.dbuser
+  password               = var.dbpassword
+  db_subnet_group_name   = var.db_subnet_group_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  identifier             = var.db_identifier
+  skip_final_snapshot    = var.skip_db_snapshot
+
+  tags = {
+    Name = "mtc-db"
+  }
+}
---
+++ aws-lab/database/provider.tf
@@ -0,0 +1,14 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  alias   = "target"
+  region  = var.aws_region
+  profile = var.target_account
+}
