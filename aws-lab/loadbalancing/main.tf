76887aaf3e790a080e22b6c3b0b03d2a86939c5c
Kevin Fatkin <kfatkin@gmail.com>
2021-12-17 04:50:15 -0500

    Add in TG and LIstener

--- aws-lab/loadbalancing/main.tf
+++ aws-lab/loadbalancing/main.tf
@@ -7,3 +7,28 @@ resource "aws_lb" "mtc_lb" {
   security_groups = [var.public_sg]
   idle_timeout    = 400
 }
+
+resource "aws_lb_target_group" "mtc_tg" {
+  provider = aws.target
+  name     = "mtc-lb-tg-${substr(uuid(), 0, 5)}"
+  port     = var.tg_port
+  protocol = var.tg_protocol
+  vpc_id   = var.vpc_id
+  health_check {
+    healthy_threshold   = var.lb_healthy_threshold
+    unhealthy_threshold = var.lb_unhealthy_threshold
+    timeout             = var.lb_timeout
+    interval            = var.lb_interval
+  }
+}
+
+resource "aws_lb_listener" "mtc_lb_listener" {
+  provider          = aws.target
+  load_balancer_arn = aws_lb.mtc_lb.arn
+  port              = var.listener_port
+  protocol          = var.listener_protocol
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mtc_tg.arn
+  }
+}
